d4597ae4c757e853e54e6b8bfd55ef2431bb799a
ariel <ariel44r@gmail.com>
2018-04-24 12:52:17 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
