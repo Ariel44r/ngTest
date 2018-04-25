839f1a7d24eeb772b7dc9814bcd6df7fddc1bc8e
ariel <ariel44r@gmail.com>
2018-04-25 01:13:17 -0500

    add login, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CardsComponent } from './cards/cards.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  { path: '#', component: CardsComponent },
+  { path: 'login', component: LoginComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardsComponent
+    CardsComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
