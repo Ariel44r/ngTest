9e28d83fff2d7b06a78b3685ccb73788af2fcf83
ariel <ariel44r@gmail.com>
2018-04-25 14:28:17 -0500

    add signup & home components, fixed improvements into navbar template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CardsComponent } from './cards/cards.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
   { path: '#', component: CardsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignupComponent }
 ]
 
 @NgModule({
@@ -17,7 +22,9 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     CardsComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
