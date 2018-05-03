a08eb60582d398ba0f67396e1ad758e05355f453
ariel <ariel44r@gmail.com>
2018-05-03 02:01:06 -0500

    add single testing reactive forms

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,9 +8,48 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  post: any;
+  description: string;
+  name: string;
+  titleAlert:string = 'This field is required';
 
-  ngOnInit() {
+  constructor(private fBuilder: FormBuilder) { 
+
+    this.rForm = fBuilder.group({
+      'name': [
+        null,
+        Validators.required
+      ],
+      'description': [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20)
+        ]),
+      ],
+      'validate': ''
+    })
+
   }
 
+  ngOnInit() {
+    this.rForm.get('validate').valueChanges.subscribe(
+      (validate) => {
+        if (validate == '1') {
+          this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+          this.titleAlert = "You need to specify at least 3 characters";
+        } else {
+          this.rForm.get('name').setValidators(Validators.required);
+        }
+        this.rForm.get('name').updateValueAndValidity();
+      }
+    )
+  }
+
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
 }
